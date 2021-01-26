b1543c64feca53bc025afa9d7b4cdddb58e27fb7
andrewjn <fidolimp@gmail.com>
2021-01-26 14:37:35 -0600

    Added the requirement to set an upper and lower bound for the guessing game.

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace GitTest
     {
         //Seed the RNG
         static Random randInt = new Random(DateTime.Now.Millisecond);
-        static int randNum = randInt.Next(1, 101);//Number between 1 and 100, upprt bound is exclusive
+        //static int randNum = randInt.Next(1, 101);//Number between 1 and 100, upprt bound is exclusive
 
 
         static void Main(string[] args)
@@ -28,11 +28,29 @@ namespace GitTest
         void guessIt()
         {
             int userGuess = -1;
+            int lowerBound = 0;
+            int upperBound = 0;
             string tmpString;
             int numTries = 1;
 
-            Console.Write(randNum);
-            Console.Write("I'm thinking of a number between 1 and 100. Guess it:");
+            Console.Write("Choose a lower bounds for the guessing game: ");
+            tmpString = Console.ReadLine();
+            Int32.TryParse(tmpString, out lowerBound);
+
+            Console.Write("Choose an upper bounds for the guessing game: ");
+            tmpString = Console.ReadLine();
+            Int32.TryParse(tmpString, out upperBound);
+
+
+            int randNum = randInt.Next(lowerBound, upperBound + 1);//Number between user defined upper and lower bounds.
+
+
+            //Console.Write(randNum);//Debugging tool
+            Console.Write("I'm thinking of a number between ");
+            Console.Write(lowerBound);
+            Console.Write(" and ");
+            Console.Write(upperBound);
+            Console.Write(". Guess it:");
             
             tmpString = Console.ReadLine();
             Int32.TryParse(tmpString, out userGuess);//Parse the string into an integer.
@@ -48,7 +66,12 @@ namespace GitTest
                 {
                     Console.WriteLine("Too high, aim lower. ");
                 }
-                Console.Write("I'm thinking of a number between 1 and 100. Guess it:");
+                Console.Write("I'm thinking of a number between ");
+                Console.Write(lowerBound);
+                Console.Write(" and ");
+                Console.Write(upperBound);
+                Console.Write(". Guess it:");
+
                 tmpString = Console.ReadLine();
                 Int32.TryParse(tmpString, out userGuess);
 
